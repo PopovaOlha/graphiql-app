3cd4200c2dffc021a37437b13c33a0acdb0a088e
PopovaOlha <fc.odess.10@gmail.com>
2023-12-11 21:37:43 +0200

    feat: add implementation of sign in, sign up and sign out components with firebase

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,42 +1,85 @@
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, Auth } from "firebase/auth";
 import { auth } from "../../../firebase";
 
-const SignIn: React.FC = () => {
+const AuthComponent: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [signInError, setSignInError] = useState<string | null>(null);
+  const [user, setUser] = useState<any | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const handleSignIn = async () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      setSignInError(null);
+      setError(null);
+      setUser(auth.currentUser);
     } catch (error: any) {
       console.error("Error signing in:", error.message);
-      setSignInError(error.message || "An unknown error occurred");
+      setError(error.message || "An unknown error occurred");
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      setUser(null);
+    } catch (error: any) {
+      console.error("Error signing out:", error.message);
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth as Auth, email, password);
+      setError(null);
+      setUser(auth.currentUser);
+    } catch (error: any) {
+      console.error("Error signing up:", error.message);
+      setError(error.message || "An unknown error occurred");
+    }
+  };
+
+  const handlePasswordReset = async () => {
+    try {
+      await sendPasswordResetEmail(auth as Auth, email);
+      setError(null);
+      console.log("Password reset email sent.");
+    } catch (error: any) {
+      console.error("Error sending password reset email:", error.message);
+      setError(error.message || "An unknown error occurred");
     }
   };
 
   return (
     <div>
-      <h2>Sign In</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignIn}>Sign In</button>
-      
-      {signInError && <div style={{ color: "red" }}>{signInError}</div>}
+      {user ? (
+        <>
+          <p>Welcome, {user.email}</p>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleSignIn}>Sign In</button>
+          <button onClick={handleSignUp}>Sign Up</button>
+          <button onClick={handlePasswordReset}>Forgot Password?</button>
+        </>
+      )}
+
+      {error && <div style={{ color: "red" }}>{error}</div>}
     </div>
   );
 };
 
-export default SignIn;
+export default AuthComponent;
