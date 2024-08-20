d24e2a34d95b7102c0d53a23655f7cd4341b29cd
Svitlana <svitlana.grytsai@gmail.com>
2024-08-20 22:33:45 +0200

    init: setup Next.js project, add EsLint, Prettier and husky

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 85,
+    arrowParens: 'always',
+};
+
+module.exports = config;
