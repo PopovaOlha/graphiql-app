b1253d7aa6a11203d562ff6059566a18a48c23f9
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-08-25 22:22:33 +0200

    refactor: replace translation provider, change select control

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,5 @@
-import { TranslationsProvider } from '@/components/TranslationsProvider';
-import initTranslations from '../i18n';
 import WelcomePage from './welcomepage/page';
 
-const i18nNamespaces: string[] = ['mainPage'];
-
-export default async function Home({
-    params: { locale },
-}: {
-    params: { locale: string };
-}) {
-    const { resources } = await initTranslations(locale, i18nNamespaces);
-
-    return (
-        <TranslationsProvider
-            namespaces={i18nNamespaces}
-            locale={locale}
-            resources={resources}
-        >
-            <WelcomePage />
-        </TranslationsProvider>
-    );
+export default async function Home() {
+    return <WelcomePage />;
 }
--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -4,12 +4,13 @@ import { usePathname, useRouter } from 'next/navigation';
 import { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
 import { i18nConfig } from '../../i18nConfig';
+import { FormControl, InputLabel, NativeSelect } from '@mui/material';
 
 const LanguageSelect = () => {
-    const { i18n, t } = useTranslation();
-    const currentLocale = i18n.language;
     const router = useRouter();
     const currentPathname = usePathname();
+    const { i18n, t } = useTranslation();
+    const currentLocale = i18n.language;
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
         const newLocale = e.target.value;
@@ -34,10 +35,22 @@ const LanguageSelect = () => {
     };
 
     return (
-        <select onChange={handleChange} value={currentLocale}>
-            <option value="en">{t('languages.en')}</option>
-            <option value="ru">{t('languages.ru')}</option>
-        </select>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel variant="standard" htmlFor="language">
+                {t('languages.label')}
+            </InputLabel>
+            <NativeSelect
+                defaultValue={currentLocale}
+                inputProps={{
+                    name: 'language',
+                    id: 'language',
+                }}
+                onChange={handleChange}
+            >
+                <option value="en">{t('languages.en')}</option>
+                <option value="ru">{t('languages.ru')}</option>
+            </NativeSelect>
+        </FormControl>
     );
 };
 
