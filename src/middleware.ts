2399361381fa4180f6dcae79fb9080ec7fac5df3
PopovaOlha <fc.odessa.10@gmail.com>
2024-08-26 14:52:02 +0300

    update: middleware to verify authorization tokens

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,20 @@
 import { i18nRouter } from 'next-i18n-router';
 import { i18nConfig } from '../i18nConfig';
 import { NextRequest } from 'next/server';
+import { verifyIdToken } from './utils/tokenUtils';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+    const token = request.headers.get('Authorization')?.split('Bearer ')[1];
+
+    if (token) {
+        try {
+            const decodedToken = await verifyIdToken(token);
+            console.log('Decoded token:', decodedToken);
+        } catch (error) {
+            console.error('Error verifying token:', error);
+        }
+    }
+
     return i18nRouter(request, i18nConfig);
 }
 
