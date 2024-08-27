3d73099ca6e9a9a3e15bff36a9c08c3e1c2a7ccf
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-08-27 02:32:46 +0200

    feat: add secondary layout test

---
+++ src/__tests__/SecondaryLayout.test.tsx
@@ -0,0 +1,25 @@
+import SecondaryLayout from '../components/SecondaryLayout/SecondaryLayout';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import '@testing-library/jest-dom';
+
+describe('Secondary Layout', () => {
+    it('Should be rendered', () => {
+        render(
+            <SecondaryLayout>
+                <span></span>
+            </SecondaryLayout>
+        );
+
+        expect(screen.getByText('GraphiQL')).toBeInTheDocument();
+    });
+    it('Should renders children', () => {
+        render(
+            <SecondaryLayout>
+                <span data-testid="test">Test</span>
+            </SecondaryLayout>
+        );
+
+        expect(screen.getByTestId('test')).toBeInTheDocument();
+    });
+});
