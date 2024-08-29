b176796d0f4834f1ef3d4881126994072d5d8a9b
PopovaOlha <fc.odessa.10@gmail.com>
2024-08-29 12:21:43 +0300

    feat: add slice for graphiql

---
+++ src/store/reducers/graphiqlSlice.ts
@@ -0,0 +1,46 @@
+import { GraphiQLState, GraphQLResponse, Header } from '@/types/interfaces';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState: GraphiQLState = {
+    endpointUrl: '',
+    sdlUrl: '',
+    query: '',
+    variables: '',
+    headers: [],
+    response: null,
+    statusCode: 0,
+};
+
+const graphiqlSlice = createSlice({
+    name: 'graphiql',
+    initialState,
+    reducers: {
+        setEndpointUrl(state, action: PayloadAction<string>) {
+            state.endpointUrl = action.payload;
+        },
+        setSdlUrl(state, action: PayloadAction<string>) {
+            state.sdlUrl = action.payload;
+        },
+        setQuery(state, action: PayloadAction<string>) {
+            state.query = action.payload;
+        },
+        setVariables(state, action: PayloadAction<string>) {
+            state.variables = action.payload;
+        },
+        addHeader(state, action: PayloadAction<Header>) {
+            state.headers.push(action.payload);
+        },
+        removeHeader(state, action: PayloadAction<number>) {
+            state.headers.splice(action.payload, 1);
+        },
+        setResponse(state, action: PayloadAction<GraphQLResponse | null>) {
+            state.response = action.payload;
+        },
+        setStatusCode(state, action: PayloadAction<number>) {
+            state.statusCode = action.payload;
+        },
+    },
+});
+
+export const { setResponse } = graphiqlSlice.actions;
+export default graphiqlSlice.reducer;
