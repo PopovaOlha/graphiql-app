c2011465f0ad2fe4ab5b5fbaf53700d7d63c312c
Svitlana <svitlana.grytsai@gmail.com>
2024-09-04 09:58:31 +0200

    fix: remove unused imports and files

--- src/utils/tokenUtils.ts
+++
@@ -1,5 +0,0 @@
-import { firebaseAdmin } from '../services/firebaseAdmin';
-
-export const verifyIdToken = async (idToken: string) => {
-    return await firebaseAdmin.auth().verifyIdToken(idToken);
-};
