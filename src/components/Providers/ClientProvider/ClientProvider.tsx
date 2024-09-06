9ff7e1f57a00e0148a8a44d09f332dd40df7ec0f
PopovaOlha <fc.odess.10@gmail.com>
2024-09-06 15:34:00 +0300

    fix: delete redux provider

--- src/components/Providers/ClientProvider/ClientProvider.tsx
+++ src/components/Providers/ClientProvider/ClientProvider.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import { FC } from 'react';
-import { Provider } from 'react-redux';
 
 import { TranslationsProvider } from '@/components/Providers/TranslationsProvider';
-import { store } from '@/store/store';
 import { AppThemeProvider } from '@/theme/AppThemeProvider';
 import { ClientProviderProps } from '@/types/interfaces';
 
@@ -15,17 +13,15 @@ const ClientProvider: FC<ClientProviderProps> = ({
     i18nNamespaces,
 }) => {
     return (
-        <Provider store={store}>
-            <AppThemeProvider>
-                <TranslationsProvider
-                    namespaces={i18nNamespaces}
-                    locale={locale}
-                    resources={resources}
-                >
-                    {children}
-                </TranslationsProvider>
-            </AppThemeProvider>
-        </Provider>
+        <AppThemeProvider>
+            <TranslationsProvider
+                namespaces={i18nNamespaces}
+                locale={locale}
+                resources={resources}
+            >
+                {children}
+            </TranslationsProvider>
+        </AppThemeProvider>
     );
 };
 
