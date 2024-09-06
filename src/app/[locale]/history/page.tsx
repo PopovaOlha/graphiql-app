ab3eaf7ece8654e336e26116472adb0602ad4b3e
PopovaOlha <fc.odess.10@gmail.com>
2024-09-07 00:31:02 +0300

    fix: change logic for delete history when user logout

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -1,19 +1,27 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Button, Typography } from '@mui/material';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Box, Button, Typography } from '@mui/material';
 
 import useUnauthorizedRedirect from '@/hooks/useUnauthorizedRedirect';
+import { auth } from '@/services/firebase';
+import styles from '@/styles/history.module.scss';
 
 const History = () => {
     useUnauthorizedRedirect();
     const [savedApis, setSavedApis] = useState<string[]>([]);
     const [showSavedApis, setShowSavedApis] = useState(false);
+    const [user] = useAuthState(auth);
 
     useEffect(() => {
-        const saved = JSON.parse(localStorage.getItem('savedApis') || '[]');
-        setSavedApis(saved);
-    }, []);
+        if (!user) {
+            localStorage.removeItem('savedApis');
+        } else {
+            const saved = JSON.parse(localStorage.getItem('savedApis') || '[]');
+            setSavedApis(saved);
+        }
+    }, [user]);
 
     const handleShowApis = () => {
         setShowSavedApis(true);
@@ -21,22 +29,33 @@ const History = () => {
 
     return (
         <div>
-            <Button variant="contained" onClick={handleShowApis}>
-                GraphQL
-            </Button>
-            {showSavedApis && (
-                <div>
-                    {savedApis.length > 0 ? (
-                        savedApis.map((apiUrl, index) => (
-                            <div key={index} style={{ margin: '10px 0' }}>
-                                <Typography>{apiUrl}</Typography>
-                            </div>
-                        ))
-                    ) : (
-                        <Typography>No saved APIs found.</Typography>
-                    )}
-                </div>
-            )}
+            <Box className={styles.container}>
+                <Typography className={styles.title}>
+                    History of Saved GraphQL APIs
+                </Typography>
+                <Button
+                    className={styles.button}
+                    variant="contained"
+                    onClick={handleShowApis}
+                >
+                    GraphQL
+                </Button>
+                {showSavedApis && (
+                    <Box className={styles.savedApisContainer}>
+                        {savedApis.length > 0 ? (
+                            savedApis.map((apiUrl, index) => (
+                                <Box key={index} className={styles.apiItem}>
+                                    <Typography>{apiUrl}</Typography>
+                                </Box>
+                            ))
+                        ) : (
+                            <Typography className={styles.noApis}>
+                                No saved APIs found.
+                            </Typography>
+                        )}
+                    </Box>
+                )}
+            </Box>
         </div>
     );
 };
