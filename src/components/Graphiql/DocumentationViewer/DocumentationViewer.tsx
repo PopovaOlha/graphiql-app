df98acb9c1dba732f1e5837a86bb7edf0a71dafa
PopovaOlha <fc.odess.10@gmail.com>
2024-09-14 22:36:29 +0300

    fix

--- src/components/Graphiql/DocumentationViewer/DocumentationViewer.tsx
+++ src/components/Graphiql/DocumentationViewer/DocumentationViewer.tsx
@@ -1,27 +1,41 @@
 import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { GraphQLField, GraphQLObjectType, GraphQLSchema } from 'graphql';
 
 interface DocumentationViewerProps {
     schema: GraphQLSchema;
 }
 
+const renderField = (field: GraphQLField<string, string>) => {
+    if (field.type instanceof GraphQLObjectType) {
+        const subFields = field.type.getFields();
+        return (
+            <ul>
+                {Object.keys(subFields).map((subFieldName) => (
+                    <li key={subFieldName}>
+                        <strong>{subFieldName}</strong>:{' '}
+                        {subFields[subFieldName].type.toString()}
+                        {renderField(subFields[subFieldName])}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+    return null;
+};
+
 const DocumentationViewer: FC<DocumentationViewerProps> = ({ schema }) => {
     const queryType = schema.getQueryType() as GraphQLObjectType;
     const fields = queryType.getFields();
 
-    const { t } = useTranslation();
-
     return (
         <Box mt={4}>
-            <h3>{t('graphqlClient:docs')}</h3>
             <ul>
                 {Object.keys(fields).map((fieldName) => (
                     <li key={fieldName}>
                         <strong>{fieldName}</strong>:{' '}
                         {fields[fieldName].type.toString()}
-                        {fields[fieldName].type === GraphQLString ? ' (String)' : ''}
+                        {renderField(fields[fieldName])}
                     </li>
                 ))}
             </ul>
--- src/components/Graphiql/GraphiQLClient/GraphiQLClient.tsx
+++ src/components/Graphiql/GraphiQLClient/GraphiQLClient.tsx
@@ -19,6 +19,7 @@ import { useParams, usePathname, useSearchParams } from 'next/navigation';
 import DocumentationViewer from '@/components/Graphiql/DocumentationViewer/DocumentationViewer';
 import { HeadersSection } from '@/components/Graphiql/HeadersSection/HeadersSection';
 import { VariablesSection } from '@/components/Graphiql/VariableSection/VariableSection';
+import { ResponseStatusIndicator } from '@/components/RestClient/RestClientComponents';
 import { executeGraphQLQuery } from '@/services/graphiqlService';
 import { addToHistory } from '@/services/historyService';
 import { fetchGraphQLSchema } from '@/services/schemaService';
@@ -26,7 +27,6 @@ import { GraphQLResponse, KeyValuePair, Variable } from '@/types/interfaces';
 import { prettifyQuery } from '@/utils/prettifyQuery';
 
 Base64.extendBuiltins();
-import { ResponseStatusIndicator } from '@/components/RestClient/RestClientComponents';
 
 const GraphiQLClient: FC<{ body: string }> = ({ body }) => {
     const pathname = usePathname();
@@ -70,7 +70,7 @@ const GraphiQLClient: FC<{ body: string }> = ({ body }) => {
             setQuery(savedQuery);
             setVariables(savedVariables);
         }
-    }, []);
+    }, [body, searchParams]);
 
     const [tabValue, setTabValue] = useState<number>(0);
 
@@ -84,7 +84,11 @@ const GraphiQLClient: FC<{ body: string }> = ({ body }) => {
     }, [endpointUrl]);
 
     useEffect(() => {
-        const newPath = `/graphiql/GRAPHQL/${endpointUrl.toBase64URL()}${query?.length ? `/${JSON.stringify({ query, variables }).toBase64URL()}` : ''}?${searchParams.toString()}`;
+        const newPath = `/graphiql/GRAPHQL/${endpointUrl.toBase64URL()}${
+            query?.length
+                ? `/${JSON.stringify({ query, variables }).toBase64URL()}`
+                : ''
+        }?${searchParams.toString()}`;
 
         window.history.pushState({}, '', newPath);
     }, [endpointUrl, searchParams, query, variables]);
@@ -254,9 +258,9 @@ const GraphiQLClient: FC<{ body: string }> = ({ body }) => {
                 <TextField
                     label={t('graphqlClient:sdlUrl')}
                     value={sdlUrl}
+                    onChange={(e) => setSdlUrl(e.target.value)}
                     fullWidth
                     margin="normal"
-                    disabled
                 />
 
                 <Box sx={{ borderBottom: 1, borderColor: 'divider', marginTop: 2 }}>
@@ -341,7 +345,14 @@ const GraphiQLClient: FC<{ body: string }> = ({ body }) => {
                         }}
                     />
                 </Box>
-                {schema && <DocumentationViewer schema={schema} />}
+                {schema && (
+                    <Box mt={4}>
+                        <Typography variant="h6">
+                            {t('graphqlClient:documentation')}
+                        </Typography>
+                        <DocumentationViewer schema={schema} />
+                    </Box>
+                )}
             </Box>
         </Container>
     );
